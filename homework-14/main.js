6e4375484a81e161bf141bb54307e006db63ce15
Nataly Dugasova <dugasova@gmail.com>
2022-11-20 21:35:48 +0200

    added error checking

--- homework-14/main.js
+++ homework-14/main.js
@@ -3,13 +3,8 @@ const outputYear = document.getElementById("input1");
 const getCentury = document.getElementById("btn");
 const message = document.getElementById("result");
 
-getCentury.addEventListener("click", () => {
-  message.innerHTML = `Entered ${outputYear.value} year is ${yearToCentury(+outputYear.value)} century`;
-});
-
-function yearToCentury(year) {
+function yearToCentury(year) { 
   let century = Math.floor(year / 100) + 1;
-
   switch (year % 10) {
     case 1:
       century = century + "st";
@@ -23,19 +18,21 @@ function yearToCentury(year) {
     default:
       century = century + "th";
   }
-  return century;
+    return century;
 }
+
+getCentury.addEventListener("click", () => {
+  if (isNaN(outputYear.value) || +outputYear.value < 1) {
+    message.innerHTML = 'Please enter positive number'
+  } else {
+    message.innerHTML = ` Entered ${outputYear.value} year is ${yearToCentury(+outputYear.value)} century`;
+  }
+ });
 
 // 2
 const enteredStr = document.getElementById("input2");
 const getString = document.getElementById("btn2");
 const outputNumber = document.getElementById("output-number");
-
-getString.addEventListener("click", () => {
-  outputNumber.innerHTML = `The result of ${enteredStr.value} is ${vowelOne(
-    enteredStr.value
-  )}`;
-});
 
 function vowelOne(str) {
   const vowels = ["a", "e", "i", "o", "u"];
@@ -49,17 +46,16 @@ function vowelOne(str) {
   }
   return result;
 }
+getString.addEventListener("click", () => {
+  outputNumber.innerHTML = `The result of ${enteredStr.value} is ${vowelOne(
+    enteredStr.value
+  )}`;
+});
 
 // 3
 const enteredPhrase = document.getElementById("input3");
 const getPhrase = document.getElementById("btn3");
 const reversedSubString = document.getElementById("reversed");
-
-getPhrase.addEventListener("click", () => {
-  reversedSubString.innerHTML = `It is a new str ${spinWords(
-    enteredPhrase.value
-  )}`;
-});
 
 function spinWords(str) {
   let newString = "";
@@ -75,39 +71,39 @@ function spinWords(str) {
   });
   return newString;
 }
+getPhrase.addEventListener("click", () => {
+  reversedSubString.innerHTML = `It is a new str ${spinWords(
+    enteredPhrase.value
+  )}`;
+});
 
 // 4
 const inputNumber = document.getElementById("input4");
 const getNumber = document.getElementById("btn4");
 const outputResult = document.getElementById("high-and-low");
 
-getNumber.addEventListener("click", () => {
-  outputResult.innerHTML = `From array of numbers the highest and lowest numbers is ${highAndLow(
-    inputNumber.value
-  )}`;
-});
-
 function highAndLow(inputString) {
-  numbers = inputString.split(" ");
+  const numbers = inputString.split(" ");
   numbers.sort((a, b) => b - a);
   return `${numbers[0]} ${numbers[numbers.length - 1]}`;
 };
+getNumber.addEventListener("click", () => {
+  if (isNaN(+inputNumber.value)) {
+    outputResult.innerHTML = 'Please enter number'
+  }else 
+  outputResult.innerHTML = `From array of numbers the highest and lowest numbers is ${highAndLow(
+    inputNumber.value
+   
+  )}`;
+});
 
 // 5
 const inputNumbers = document.getElementById("input5");
 const getForPhone = document.getElementById("btn5");
 const outputPhone = document.getElementById("phone-number");
 
-getForPhone.addEventListener("click", () => {
-  let numbersArray = inputNumbers.value.split(" ");
-  numbersArray = numbersArray.map((item) => +item);
-
-  outputPhone.innerHTML = `The phone number is ${createPhoneNumber(
-    numbersArray
-  )}`;
-});
-
 function createPhoneNumber(numbersArray) {
+ 
   const numbersStr = numbersArray.join("");
   return (
     "(" +
@@ -118,3 +114,15 @@ function createPhoneNumber(numbersArray) {
     numbersStr.slice(6)
   );
 };
+getForPhone.addEventListener("click", () => {
+  if (isNaN(+inputNumbers.value)) {
+    outputPhone.innerHTML = 'Please enter number'
+    return
+  } 
+  let numbersArray = inputNumbers.value.split("");
+  numbersArray = numbersArray.map((item) => +item);
+  
+  outputPhone.innerHTML = `The phone number is ${createPhoneNumber(
+    numbersArray
+  )}`;
+});
