a6528eacbf776bf90776d07cad0a4ce11dac6950
Nataly Dugasova <dugasova@gmail.com>
2022-12-01 12:06:17 +0200

    Added event delegation, style for first page

--- homework-18/main.js
+++ homework-18/main.js
@@ -8,20 +8,26 @@ const filmDetails = document.getElementById("details");
 function ShowPagination(totalMovies) {
   if (
     document.getElementById("pagination").getElementsByTagName("li").length > 0
-  )
+  ) {
     return;
+  }
 
   pagination.innerHTML = "";
 
   for (let i = 0; i * MOVIES_PER_PAGE < totalMovies && i < 15; i++) {
     const li = document.createElement("li");
     li.innerHTML = i + 1;
+    if (i === 0) {
+      li.classList.add("active");
+    }
     pagination.append(li);
   }
 }
 
 function showDetails(event) {
-  moreDetails(event.target.dataset.movieId);
+  if (event.target.dataset.movieId) {
+    moreDetails(event.target.dataset.movieId);
+  }
 }
 
 async function loadData(page) {
@@ -34,7 +40,7 @@ async function loadData(page) {
   }
 
   let filmUrl =
-    "https://www.omdbapi.com/?apikey=a0d12fb1&s=" + searchInput.value;
+    `https://www.omdbapi.com/?apikey=a0d12fb1&s=${searchInput.value}`;
 
   if (page > 0) {
     filmUrl += "&page=" + page;
@@ -47,7 +53,7 @@ async function loadData(page) {
     movies = movies.data;
   } catch (err) {
     searchResults.innerHTML =
-      "<h2 class = header-not-found>Exception in Promise!!!</h2>" + err.message;
+      "<h2 class = header-not-found>Something went wrong</h2>" + err.message;
     return;
   }
 
@@ -62,22 +68,12 @@ async function loadData(page) {
 
   searchResults.innerHTML = "";
   movies.Search.forEach((item) => {
-    searchResults.innerHTML +=
-      "<div class=result>" +
-      (item.Poster !== "N/A" ? "<img  src= " + item.Poster + ">" : "") +
-      item.Title +
-      " " +
-      item.Type +
-      " " +
-      item.Year +
-      '<button class="details" data-movie-id="' +
-      item.imdbID +
-      '">More</button></div>';
-  });
-
-  const detailsButtons = document.querySelectorAll(".details");
-  detailsButtons.forEach((item) => {
-    item.addEventListener("click", showDetails);
+    searchResults.innerHTML += `<div class="result"><img src=${
+      item.Poster !== "N/A" ? item.Poster : "/homework-18/img/movie.jpg"
+    }>
+        ${item.Title}<br>${item.Type}<br>${
+      item.Year
+    }<button class="details" data-movie-id=${item.imdbID}>More</button></div>`;
   });
 }
 
@@ -85,20 +81,20 @@ async function moreDetails(movieId) {
   filmDetails.innerHTML = "";
   moreDetails.innerHTML = "";
 
-  const moreFilmUrl = "https://www.omdbapi.com/?apikey=a0d12fb1&i=" + movieId;
+  const moreFilmUrl = `https://www.omdbapi.com/?apikey=a0d12fb1&i=${movieId}`;
   let moreData = null;
   try {
     moreData = (await axios.get(moreFilmUrl)).data;
   } catch (err) {
     searchResults.innerHTML =
-      "<h2 class = header-not-found>Exception in Promise!!!</h2>" + err.message;
+      "<h2 class = header-not-found>Something went wrong!!!</h2>" + err.message;
     return;
   }
 
   filmDetails.innerHTML =
     (moreData.Poster !== "N/A"
       ? `<img class="data-result" src=${moreData.Poster}>`
-      : "") +
+      : `<img src="/homework-18/img/movie.jpg">`) +
     `<div class ="data-about"<br> Title: ${moreData.Title}<br> Relised: ${moreData.Released}<br>
   Country: ${moreData.Country}<br>Actors: ${moreData.Actors}<br> 
   Genre:${moreData.Genre}<br> Runtime:${moreData.Runtime}<br> Plot:${moreData.Plot}<br>
@@ -111,13 +107,18 @@ getFilm.addEventListener("click", () => {
 });
 
 pagination.addEventListener("click", (e) => {
-  const pages = document.querySelectorAll("ul li");
+  if (!(e.target instanceof HTMLLIElement)) return;
+
+  const pages = pagination.querySelectorAll("li");
 
   pages.forEach((item) => {
     item.classList.remove("active");
   });
 
-  pageIndex = +e.target.innerHTML - 1;
   e.target.classList.add("active");
+
+  const pageIndex = +e.target.innerHTML - 1;
   loadData(pageIndex);
 });
+
+searchResults.addEventListener("click", showDetails);
