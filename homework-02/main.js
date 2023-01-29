2fbd8214e07004d0c801aaa0581afc38ef8be6bf
Nataly Dugasova <83303958+dugasova@users.noreply.github.com>
2023-01-29 19:50:53 +0200

    Homework 02

---
+++ homework-02/main.js
@@ -0,0 +1,31 @@
+// a.
+const MY_Name = 'Nataly';
+// b.
+const MY_Surname = 'Dugasova';
+// c.
+const MY_City = 'Sevastopol';
+// d.
+let average = (2 + 3 + 6 + 5) / 4;
+// e.
+let circleRadius = 6378.1;
+// f.
+let circumference, circleArea;
+// g. 
+circumference = 2 * Math.PI * circleRadius;
+circleArea = Math.PI * Math.pow(circleRadius, 2);
+// h. 
+// let 123Error, error!, variable withSpace; 
+
+// i. 
+// This code will do this thing (...) and that thing (...)
+/*
+This
+is my long long
+comment
+*/
+
+console.log(MY_Name, MY_Surname, MY_City);
+console.log(average);
+console.log(circleRadius);
+console.log(circumference);
+console.log(circleArea);
